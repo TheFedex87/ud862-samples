5007009acbf4761cfc8f379b9a78d1c323e145c7
Federico Creti <f.creti@smre.it>
2018-05-15 11:22:06 +0200

    Added new transformation, burger to arrow, on TickCross

--- TickCross/app/src/main/java/com/example/android/tickcross/MainActivity.java
+++ TickCross/app/src/main/java/com/example/android/tickcross/MainActivity.java
@@ -9,9 +9,13 @@ import android.widget.ImageView;
 public class MainActivity extends Activity {
 
     private ImageView tickCross;
+    private ImageView burgerArrow;
     private AnimatedVectorDrawable tickToCross;
     private AnimatedVectorDrawable crossToTick;
+    private AnimatedVectorDrawable burgerToArrow;
+    private AnimatedVectorDrawable arrowToBurger;
     private boolean tick = true;
+    private boolean burger = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +24,23 @@ public class MainActivity extends Activity {
         tickCross = (ImageView) findViewById(R.id.tick_cross);
         tickToCross = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_tick_to_cross);
         crossToTick = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_cross_to_tick);
+
+        burgerArrow = findViewById(R.id.burger_arrow);
+        burgerToArrow = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_burger_to_arrow);
+        arrowToBurger = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_arrow_to_burger);
     }
 
-    public void animate(View view) {
+    public void animate_tick_cross(View view) {
         AnimatedVectorDrawable drawable = tick ? tickToCross : crossToTick;
         tickCross.setImageDrawable(drawable);
         drawable.start();
         tick = !tick;
+    }
+
+    public void animate_burger_arrow(View view) {
+        AnimatedVectorDrawable drawable = burger ? burgerToArrow : arrowToBurger;
+        burgerArrow.setImageDrawable(drawable);
+        drawable.start();
+        burger = !burger;
     }
 }
